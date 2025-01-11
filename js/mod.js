b981a2439b6f80a42a3537e376f906c10a4502fc
ThePrestigeTreeGuy <maxim.alexandrov777@gmail.com>
2025-01-11 13:58:39 -0500

    v0.0.1
    yay

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The Doors Tree",
-	id: "37258649213278",
-	author: "ThePrestigeTreeGuy/Gaming And Walkthroughs on discord",
+	name: "The Doors Tree 2",
+	id: "372586492413278",
+	author: "ThePrestigeTreeGuy/Gaming And Walkthroughs on discord/gaw on galaxy.click",
 	pointsName: "studs",
 	modFiles: ["layers.js", "tree.js"],
 
@@ -13,61 +13,16 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.7",
-	name: "Rebounding",
+	num: "0.0.1",
+	name: "The Start",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.7</h3><br>
-		- Added a new layer!<br>
-		- Added 32 upgrades!<br>
-		- Added 16 milestones!<br>
-		- Added 12 buyables!<br>
-		- Added a challenge!<br>
-		- Added achievements! (note: only unlocked when new layer is unlocked)<br>
-		- CSS changes!<br>
-		- Endgame: e4.2e6 points<br>
-	<h3>v0.6.1</h3><br>
-		- added 35 upgrades<br>
-		- added a challenge<br>
-		- Endgame: e13600 points (Dupe 10, pre-??? <- new layer)<br>
-	<h3>v0.6</h3><br>
-		- added 2 LAYERS????<br>
-		- Added 18 upgrades!!!<br>
-		- automation.<br>
-		- Endgame: e215 studs.<br>
-	<h3>v0.5.1.1</h3><br>
-		- fixed a bug<br>
-	<h3>v0.5.1</h3><br>
-		- an update for sure<br>
-		- More content in dupe layer.<br>
-		- Added 4 upgrades.<br>
-		- Endgame: Dupe 4.<br>
-		- Bug fixing!<br>
-	<h3>v0.5</h3><br>
-		- smaller update here<br>
-		- Added  5th layer, dupe<br>
-		- 6 more upgrades<br>
-		- More milestones<br>
-		- Endgame: Dupe Milestone 2<br>
-	<h3>v0.4</h3><br>
-		- Added the fourth layer, RUSH<br>
-		- Added 10 upgrades!<br>
-		- Some design improvements!<br>
-		- Expanded 1st row!<br>
-		- Endgame: e30 studs.<br>
-	<h3>v0.3</h3><br>
-		- Added the third layer, knobs.<br>
-		- Added 5 upgrades!<br>
-		- Endgame: Knob Upgrade 3.<br>
-	<h3>v0.2</h3><br>
-		- Added the second layer, closets.<br>
-		- Added an upgrade.<br>
-		- Endgame: 10 closets.<br>
-	<h3>v0.1</h3><br>
-		- Added the first layer, doors.<br>
-		- Added an upgrade.<br>
-		- Endgame: 5 doors.`
+	<h3>v0.0.1</h3><br>
+		- Game Release<br>
+		- Added 2 Layers<br>
+		- Endgame: 1 billion studs.
+`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
@@ -86,86 +41,14 @@ function canGenPoints(){
 
 // Calculate points/sec!
 function getPointGen() {
-	if(!canGenPoints()
-		)return new Decimal(0)
+	if(!canGenPoints())
+	return new Decimal(0)
 	let gain = new Decimal(1)
 	if (hasUpgrade('d', 11)) gain = gain.times(upgradeEffect('d', 11))
 	if (hasUpgrade('d', 12)) gain = gain.times(upgradeEffect('d', 12))
-	if (hasUpgrade('d', 13)) gain = gain.times(upgradeEffect('d', 13))
-	if (hasUpgrade('d', 14)) gain = gain.times(upgradeEffect('d', 14))
-	if (hasUpgrade('d', 15)) gain = gain.times(upgradeEffect('d', 15))
-	if (hasUpgrade('d', 34)) gain = gain.times(1e5)
-	if (hasUpgrade('d', 43)) gain = gain.times(upgradeEffect('d', 43))
-	if (hasUpgrade('c', 11)) gain = gain.times(10)
+	gain = gain.times(buyableEffect('d', 11))
+	if (hasUpgrade('c', 11)) gain = gain.times(5)
 	if (hasUpgrade('c', 12)) gain = gain.times(upgradeEffect('c', 12))
-	if (hasUpgrade('c', 13)) gain = gain.times(upgradeEffect('c', 13))
-	if (hasUpgrade('c', 22)) gain = gain.times(upgradeEffect('c', 22))
-	if (hasUpgrade('c', 23)) gain = gain.times(upgradeEffect('c', 23))
-	if (hasUpgrade('c', 25)) gain = gain.times(100)
-	if (hasUpgrade('c', 35)) gain = gain.times(100)
-	if (hasUpgrade('k', 11)) gain = gain.times(10)
-	if (hasUpgrade('k', 12)) gain = gain.times(upgradeEffect('k', 12))
-	if (hasUpgrade('k', 13)) gain = gain.times(upgradeEffect('k', 13))
-	if (hasUpgrade('k', 14)) gain = gain.times(upgradeEffect('k', 14))
-	if (hasUpgrade('k', 15)) gain = gain.times(upgradeEffect('k', 15))
-	if (hasUpgrade('k', 34)) gain = gain.times(upgradeEffect('k', 34))
-	if (hasUpgrade('r', 11)) gain = gain.times(10)
-	if (hasUpgrade('r', 12)) gain = gain.times(upgradeEffect('r', 12))
-	if (hasUpgrade('r', 15)) gain = gain.times(upgradeEffect('r', 15))
-	if (hasUpgrade('r', 22)) gain = gain.times(upgradeEffect('r', 22))
-	if (hasUpgrade('r', 23)) gain = gain.times(upgradeEffect('r', 23))
-	if (hasUpgrade('r', 31)) gain = gain.times(1e20)
-	if (hasMilestone('du', 1)) gain = gain.times(100)
-	if (hasUpgrade('du', 12)) gain = gain.times(upgradeEffect('du', 12))
-	if (hasUpgrade('du', 13)) gain = gain.times(upgradeEffect('du', 13))
-	if (hasUpgrade('du', 14)) gain = gain.times(upgradeEffect('du', 14))
-	if (hasUpgrade('du', 15)) gain = gain.times(upgradeEffect('du', 15))
-	if (hasUpgrade('du', 21)) gain = gain.times(upgradeEffect('du', 21))
-	if (hasMilestone('f', 1)) gain = gain.times(100)
-	if (hasMilestone('f', 4)) gain = gain.times(tmp['f'].milestones[4].effect)
-	if (hasUpgrade('f', 15)) gain = gain.times(upgradeEffect('f', 15))
-	if (hasUpgrade('g', 21)) gain = gain.times(upgradeEffect('g', 21))
-	if (hasUpgrade('g', 23)) gain = gain.times(upgradeEffect('g', 23))
-	if (hasUpgrade('g', 24)) gain = gain.times(upgradeEffect('g', 24))
-	if (hasUpgrade('g', 31)) gain = gain.times(upgradeEffect('g', 31))
-	if (hasMilestone('g', 1)) gain = gain.times(100)
-	if (hasMilestone('amb', 2)) gain = gain.times(tmp['amb'].milestones[2].effect)
-	if (hasMilestone('r', 4)) gain = gain.times(tmp['r'].milestones[4].effect)
-	if (hasUpgrade('g', 12)) gain = gain.pow(1.05)
-	if (hasUpgrade('g', 13)) gain = gain.pow(1.05)
-	if (hasUpgrade('g', 14)) gain = gain.pow(1.05)
-	if (hasUpgrade('g', 32)) gain = gain.pow(1.02)
-	if (hasUpgrade('g', 15)) gain = gain.pow(upgradeEffect('g', 15))
-	if (hasUpgrade('g', 34)) gain = gain.pow(upgradeEffect('g', 34))
-	if (hasUpgrade('k', 24)) gain = gain.pow(1.05)
-	if (hasUpgrade('k', 25)) gain = gain.pow(1.05)
-	if (hasUpgrade('d', 31)) gain = gain.pow(1.05)
-	if (hasUpgrade('c', 31)) gain = gain.pow(1.05)
-	if (hasUpgrade('f', 11)) gain = gain.pow(1.05)
-	if (hasUpgrade('f', 13)) gain = gain.pow(1.05)
-	if (hasUpgrade('c', 34)) gain = gain.pow(upgradeEffect('c', 34))
-	if (inChallenge('r',11)) gain = gain.pow(0.5)
-	if (hasChallenge('r',11)) gain = gain.pow(1.05)
-	if (inChallenge('amb',11)) gain = gain.pow(0.1)
-	if (hasChallenge('amb',11)) gain = gain.pow(1.02)
-	if (hasUpgrade('du',22)) gain = gain.pow(1.03)
-	if (hasUpgrade('du',23)) gain = gain.pow(1.03)
-	if (hasUpgrade('du',24)) gain = gain.pow(1.03)
-	if (hasUpgrade('amb',11)) gain = gain.pow(1.02)
-	if (hasUpgrade('amb',12)) gain = gain.pow(1.02)
-	if (hasUpgrade('amb',13)) gain = gain.pow(1.02)
-	if (hasUpgrade('amb',14)) gain = gain.pow(1.04)
-	if (hasUpgrade('amb',15)) gain = gain.pow(1.01)
-	if (hasUpgrade('d',41)) gain = gain.pow(0.9)
-	if (hasUpgrade('d',45)) gain = gain.pow(0.9)
-	if (hasUpgrade('d',53)) gain = gain.pow(0.97)
-	if (hasUpgrade('d',54)) gain = gain.pow(0.97)
-	if (hasUpgrade('d',55)) gain = gain.pow(0.97)
-	if (hasUpgrade('c',42)) gain = gain.pow(1.005)
-	if (hasMilestone('c',3)) gain = gain.pow(0.93)
-	if (hasMilestone('d',2)) gain = gain.pow(0.95)
-	gain = gain.pow(buyableEffect('c', 12))
-	gain = gain.pow(buyableEffect('r', 12))
 	return gain
 }
 
@@ -179,7 +62,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e4200000"))
+	return player.points.gte(new Decimal("e9"))
 }
 
 
